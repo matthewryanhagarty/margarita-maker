3eb435efe2da35444c2b603b4133b2688bcf66ee
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-07-07 21:14:34 -0700

    continuing to add files and their respective code.

---
+++ controllers/mixologist_controller.js
@@ -0,0 +1,43 @@
+var express = require("express");
+
+var router = express.Router();
+
+var mixologist = require("../models/mixologist.js")
+
+router.get("/", function(req, res) {
+    mixologist.all(function(data) {
+      var hbsObject = {
+        ingredients: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/ingredients", function(req, res) {
+    mixologist.create([
+      "ingredient_name", "used"
+    ], [
+      req.body.ingredient_name, req.body.used
+    ], function(result) {
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/ingredients/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    mixologist.update({
+      notUsed: req.body.used
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+    module.exports = router;
---
+++ models/mixologist.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orm.js");
+
+var mixologist = {
+    all: function(cb) {
+      orm.all("ingredients", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("ingredients", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("ingredients", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+
+  module.exports = mixologist;
