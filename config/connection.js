930dccf52e167675a48db66dc8bcf5346b263c1d
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-07-08 14:50:28 -0700

    add JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -15,5 +20,6 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+}
 
 module.exports = connection;
