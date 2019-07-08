656ffb52bd116c5d87317849066ded9691a21cb3
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-07-08 15:02:46 -0700

    deploy new changes to heroku.

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
